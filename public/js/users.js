44979c9f3af99fc6b984035afd2bb031bef81913
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-11-27 03:41:21 -0500

    add function to save data

---
+++ public/js/users.js
@@ -0,0 +1,26 @@
+function saveUser(){
+
+   $.ajax({
+
+    url:"/user",
+    type:"post",
+    data:$("#addUser").serialize(),
+    success:function(res){
+
+        window.location.reload();
+        return false;
+    },
+    error:function(xhr, status, error){
+
+        console.log(xhr.responseText);
+        var err = '';
+        $.each(JSON.parse(xhr.responseText) , function(i, item) {
+
+             err +='<li>'+item.msg+'</li>';
+        });
+        $(".err-container").html(err);
+        return false;
+    }
+
+   });
+}
