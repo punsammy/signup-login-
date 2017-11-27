a76c6ee5677df8584fccab6b344d5bf55dcc9fd0
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-11-26 19:23:22 -0500

    configure to listen on port 8080

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(expressValidator());
+
+app.listen(process.env.PORT || 8080, function(){
+  console.log("Server is listening on port 8080");
+});
